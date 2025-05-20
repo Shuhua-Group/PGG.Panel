af715c80b71e0fb80af130c6458cf5a85a25b4b4
54sym <140576853+54sym@users.noreply.github.com>
2025-05-20 16:17:52 +0800

    Add files via upload

--- phase.sh
+++ phase.sh
@@ -4,8 +4,7 @@ i=$1
 
 prefix='POP_zyHan_HXDL'
 qc='VQSR' #qc=filtered
-script='/home/sunyumeng/script/shapeit5.sh'
 map="/home/sunyumeng/software/shapeit4-4.2.2/maps/b38/chr$i.b38.gmap.gz"
 
 vcf="../01.qc/$prefix.chr${i}.$qc.PASS.biallelic.SNPs.FM10.AC1.vcf.gz"
-sh $script --input $vcf --map $map --chr chr$i --output-prefix $prefix.chr${i}.$qc.PASS.biallelic.SNPs.FM10.AC1
+sh shapeit5.sh --input $vcf --map $map --chr chr$i --output-prefix $prefix.chr${i}.$qc.PASS.biallelic.SNPs.FM10.AC1
