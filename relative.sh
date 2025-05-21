d2b51aa943269882850b61d034d72b461342a7ac
54sym <140576853+54sym@users.noreply.github.com>
2025-05-21 12:56:42 +0800

    Add files via upload

--- relative.sh
+++ relative.sh
@@ -10,9 +10,6 @@ do
 done
 plink2 --pmerge-list merge.list.txt --make-bed --out $prefix.chrA.$qc.PASS.biallelic.SNPs.FM10.AC1
 
-#method 1
 king -b $prefix.chrA.$qc.PASS.biallelic.SNPs.FM10.AC1.bed --related --degree 3 --prefix $prefix
 grep -v 'InfType' $prefix.kin0 | awk '$14=="Dup/MZ"||$14=="PO"||$14=="FS"||$14=="2nd"{print $1,$3}' | python 5011.relative.combinations.py  /dev/stdin  rm.relative.king.list 1>rm.relative.king.log  2>&1
-# #method 2
-# plink --bfile $prefix.chrA.plink --genome -out $prefix
-# grep -v 'PI_HAT' $prefix.genome | awk '$10>=0.05{print $1,$3}' | python 5011.relative.combinations.py /dev/stdin  rm.relative.PI_HAT.list 1>rm.relative.PI_HAT.log 2>&1
+cat $prefix.chrA.$qc.PASS.biallelic.SNPs.FM10.AC1.fam  rm.relative.king.list|awk '{print $1}'|sort|uniq -u >  no.relate.list
